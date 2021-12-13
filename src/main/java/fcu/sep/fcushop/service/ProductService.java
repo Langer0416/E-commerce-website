4efd1c33de90a2a6d27fa7605587fea80095714b
“Your <frankie139506@gmail.com>
2021-12-13 11:54:06 +0800

    test

--- src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -43,10 +43,46 @@ public class ProductService {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
           + " from PRODUCT where name like :keyword";
-
       return connection.createQuery(query)
           .addParameter("keyword", "%"+keyword+"%")
           .executeAndFetch(Product.class);
+    }
+  }
+
+  public String AddProduct(String game_name, String img_url, int price, String description)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "insert into PRODUCT (NAME, IMAGE_URL, PRICE, DESCRIPTION) "
+          + "VALUES(:game_name, :img_url, :price, :description)";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("game_name", game_name)
+          .addParameter("img_url", img_url)
+          .addParameter("price", price)
+          .addParameter("description", description)
+          .executeUpdate();
+
+      return "Success";
+
+    }
+  }
+
+  public String UpdateProduct(String game_name, String img_url, int price, String description)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "Update PRODUCT "
+          + "SET IMAGE_URL= :img_url,PRICE= :price,DESCRIPTION= :description WHERE NAME = :game_name";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("game_name", game_name)
+          .addParameter("img_url", img_url)
+          .addParameter("price", price)
+          .addParameter("description", description)
+          .executeUpdate();
+      return "Success";
+
     }
   }
 
