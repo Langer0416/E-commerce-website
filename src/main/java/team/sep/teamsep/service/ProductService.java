cbf04c93a5f8d407e0c12451e8a1446075e9c33f
Felix <Langer0416@gmail.com>
2021-12-19 12:57:48 +0800

    Try merge

--- src/main/java/team/sep/teamsep/service/ProductService.java
+++ src/main/java/team/sep/teamsep/service/ProductService.java
@@ -27,26 +27,26 @@ public class ProductService {
     public List<Product> getProducts(String keyword) {
         try (Connection connection = sql2oDbHandler.getConnector().open()) {
             String query = "select PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity"
-                + " FROM project.product where PRODUCT_NAME like :keyword";
+                    + " FROM project.product where PRODUCT_NAME like :keyword";
 
             return connection.createQuery(query)
-                .addParameter("keyword", "%" + keyword + "%")
-                .executeAndFetch(Product.class);
+                    .addParameter("keyword", "%" + keyword + "%")
+                    .executeAndFetch(Product.class);
         }
     }
 
     //List<Product> first = 0;
-   public String InsertIntoCar1(long id) {
+    public String InsertIntoCar1(long id) {
         try (Connection connection = sql2oDbHandler.getConnector().open()) {
             String query = "Insert into project.productcar(PRODUCT_NAME,INSTOCK,PRICE,QUANTITY,PICTURE) select PRODUCT_NAME,INSTOCK,PRICE,QUANTITY,PICTURE FROM project.product where PRODUCT_ID = :id";
 
             System.out.println(query);
-           connection.createQuery(query)
+            connection.createQuery(query)
                     .addParameter("id",id)
                     .executeUpdate();
 
         }
-       return "success";
+        return "success";
     }
 
 
@@ -56,12 +56,12 @@ public class ProductService {
 
             System.out.println(query);
             connection.createQuery(query)
-                .addParameter("name",name)
-                .addParameter("stock",stock)
-                .addParameter("price",price)
-                .addParameter("quantity",quantity)
-                .addParameter("picture",picture)
-                .executeUpdate();
+                    .addParameter("name",name)
+                    .addParameter("stock",stock)
+                    .addParameter("price",price)
+                    .addParameter("quantity",quantity)
+                    .addParameter("picture",picture)
+                    .executeUpdate();
 
             return "success";
         }
@@ -73,12 +73,12 @@ public class ProductService {
 
 
             connection.createQuery(query)
-                .addParameter("name",name)
-                .addParameter("stock",stock)
-                .addParameter("price",price)
-                .addParameter("quantity",quantity)
-                .addParameter("picture",picture)
-                .executeUpdate();
+                    .addParameter("name",name)
+                    .addParameter("stock",stock)
+                    .addParameter("price",price)
+                    .addParameter("quantity",quantity)
+                    .addParameter("picture",picture)
+                    .executeUpdate();
             return "Success";
         }
     }
@@ -88,9 +88,9 @@ public class ProductService {
             String query = "SELECT count(*) as total FROM project.customer WHERE ACCOUNT=:account and PASSWORD =:password";
 
             count=connection.createQuery(query)
-                .addParameter("account", account)
-                .addParameter("password", password)
-                .executeScalar(Integer.class);
+                    .addParameter("account", account)
+                    .addParameter("password", password)
+                    .executeScalar(Integer.class);
         }
         if(count>0) {
             return "Success";
@@ -106,9 +106,9 @@ public class ProductService {
             String query = "SELECT count(*) as total FROM project.seller WHERE ACCOUNT=:account and PASSWORD =:password";
 
             count1=connection.createQuery(query)
-                .addParameter("account", account)
-                .addParameter("password", password)
-                .executeScalar(Integer.class);
+                    .addParameter("account", account)
+                    .addParameter("password", password)
+                    .executeScalar(Integer.class);
         }
         if(count1>0) {
             return "Success";
@@ -118,17 +118,17 @@ public class ProductService {
         }
     }
 
-int b;
+    int b;
     public String registerProduct(String account,String name,Integer phone,String password) {
         try (Connection connection = sql2oDbHandler.getConnector().open()) {
             String query = "Insert INTO project.customer(ACCOUNT,NAME,PHONE_NUMBER,PASSWORD) VALUES(:account,:name,:phone,:password)";
 
-                b=connection.createQuery(query)
-                .addParameter("account", account)
-                .addParameter("name", name)
-                .addParameter("phone", phone)
-                .addParameter("password", password)
-                .executeUpdate()
+            b=connection.createQuery(query)
+                    .addParameter("account", account)
+                    .addParameter("name", name)
+                    .addParameter("phone", phone)
+                    .addParameter("password", password)
+                    .executeUpdate()
                     .getKey(int.class);
             if(b==0) {
                 return "success";
