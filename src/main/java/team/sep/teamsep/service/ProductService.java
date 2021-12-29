600c8989116b04de6c06ed20cb4e1d3dc4b36ac0
pzs <km9940814.3@gmail.com>
2021-12-29 15:58:22 +0800

    122902

--- src/main/java/team/sep/teamsep/service/ProductService.java
+++ src/main/java/team/sep/teamsep/service/ProductService.java
@@ -1,102 +1,124 @@
 package team.sep.teamsep.service;
 
-import team.sep.teamsep.model.Order;
-import team.sep.teamsep.model.Product;
-import team.sep.teamsep.database.Sql2oDbHandler;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
+import team.sep.teamsep.database.Sql2oDbHandler;
+import team.sep.teamsep.model.Order;
+import team.sep.teamsep.model.Product;
 import team.sep.teamsep.model.ShopCar;
-
-import java.util.List;
 
 @Service
 public class ProductService {
-    @Autowired
-    private Sql2oDbHandler sql2oDbHandler;
+  @Autowired
+  private Sql2oDbHandler sql2oDbHandler;
 
-    public ProductService() {
+  public ProductService() {
 
-    }
-    public List<Product> getCountProducts() {
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "SELECT PRODUCT_ID id,PRODUCT_NAME name,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity FROM project.product ";
+  }
+
+  public List<Product> getCountProducts() {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "SELECT PRODUCT_ID id,PRODUCT_NAME name,INSTOCK"
+          +
+                " stock,PICTURE picture,PRICE price,QUANTITY quantity FROM project.product ";
 
 
-            return connection.createQuery(query)
+      return connection.createQuery(query)
                 .executeAndFetch(Product.class);
-        }
     }
-    public List<Product> getProducts(long id) {
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "SELECT PRODUCT_ID id,PRODUCT_NAME name,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity FROM project.product "
-                + "where PRODUCT_ID <= :id and PRODUCT_ID >= :id-5";
+  }
 
-            //System.out.println(id);
-            return connection.createQuery(query)
-                .addParameter("id",id)
+  public List<Product> getProducts(long id) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "SELECT PRODUCT_ID id,PRODUCT_NAME name,INSTOCK stock,PICTURE picture,"
+                +
+                "PRICE price,QUANTITY quantity FROM project.product "
+                +
+                "where PRODUCT_ID <= :id and PRODUCT_ID >= :id-5";
+
+      //System.out.println(id);
+      return connection.createQuery(query)
+                .addParameter("id", id)
                 .executeAndFetch(Product.class);
-        }
     }
+  }
 
   public List<ShopCar> getProducts1(String account) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
-      String query = "SELECT PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity FROM project.productcar"
-          +" where ACCOUNT =:account" ;
+      String query = "SELECT PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,"
+          +
+          "PICTURE picture,PRICE price,QUANTITY quantity FROM project.productcar"
+          +
+          " where ACCOUNT =:account";
 
       return connection.createQuery(query)
-          .addParameter("account",account)
+          .addParameter("account", account)
           .executeAndFetch(ShopCar.class);
     }
   }
-    public List<ShopCar> CheckShopCar(String account,String name){
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "SELECT ACCOUNT account,PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity FROM project.productcar "
-                          + "where PRODUCT_NAME = :name and ACCOUNT =:account";
 
-            //System.out.println(query);
-            //System.out.println(account);
-            //System.out.println(name);
-            return connection.createQuery(query)
-                    .addParameter("name",name)
-                    .addParameter("account",account)
+  public List<ShopCar> CheckShopCar(String account, String name) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "SELECT ACCOUNT account,PRODUCT_NAME name,PRODUCT_ID id,"
+                +
+                "INSTOCK stock,PICTURE picture,PRICE price,QUANTITY "
+                +
+                "quantity FROM project.productcar "
+                +
+                "where PRODUCT_NAME = :name and ACCOUNT =:account";
+      //System.out.println(query);
+      //System.out.println(account);
+      //System.out.println(name);
+      return connection.createQuery(query)
+                    .addParameter("name", name)
+                    .addParameter("account", account)
                     .executeAndFetch(ShopCar.class);
-        }
     }
+  }
 
-    public List<Order> getProductOrder() {
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "select pay.account,pay.id ,pay.name,pay.pay,pay.deliver,pay.money from pay order by pay.id";
+  public List<Order> getProductOrder() {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select pay.account,pay.id ,pay.name,pay.pay,"
+          +
+          "pay.deliver,pay.money from pay order by pay.id";
 
-            return connection.createQuery(query)
+      return connection.createQuery(query)
                     .executeAndFetch(Order.class);
-        }
     }
+  }
 
+  public List<Product> getProducts(String keyword) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,"
+                +
+                "PICTURE picture,PRICE price,QUANTITY quantity"
+                +
+                " FROM project.product where PRODUCT_NAME like :keyword";
 
-    public List<Product> getProducts(String keyword) {
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "select PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity"
-                + " FROM project.product where PRODUCT_NAME like :keyword";
-
-            return connection.createQuery(query)
+      return connection.createQuery(query)
                 .addParameter("keyword", "%" + keyword + "%")
                 .executeAndFetch(Product.class);
-        }
     }
+  }
 
-    //List<Product> first = 0;
-   public List<Product> InsertIntoCar1(String id) {
-        try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            //String query = "Insert into project.productcar(PRODUCT_NAME,INSTOCK,PRICE,PICTURE) select PRODUCT_NAME,INSTOCK,PRICE,PICTURE FROM project.product where PRODUCT_NAME = :id";
+  //List<Product> first = 0;
+  public List<Product> InsertIntoCar1(String id) {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      //String query = "Insert into project.productcar(PRODUCT_NAME,INSTOCK,PRICE,PICTURE)
+      // select PRODUCT_NAME,INSTOCK,PRICE,PICTURE FROM project.product where PRODUCT_NAME = :id";
+      // String query1 ="Insert into project.productcar(QUANTITY) VALUES(:amount)
+      // where PRODUCT_NAME =':id'";
+      String query2 = "select PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,"
+                +
+                "PICTURE picture,PRICE price,QUANTITY quantity"
+                +
+                " FROM project.product where PRODUCT_NAME =:id";
 
-           // String query1 ="Insert into project.productcar(QUANTITY) VALUES(:amount) where PRODUCT_NAME =':id'";
-            String query2 = "select PRODUCT_NAME name,PRODUCT_ID id,INSTOCK stock,PICTURE picture,PRICE price,QUANTITY quantity"
-                    + " FROM project.product where PRODUCT_NAME =:id";
 
-
-            return connection.createQuery(query2)
-                    .addParameter("id",id)
+      return connection.createQuery(query2)
+                    .addParameter("id", id)
                     .executeAndFetch(Product.class);
             //System.out.println(query2);
             /*connection.createQuery(query1)
@@ -110,22 +132,25 @@ public class ProductService {
                     //.addParameter("amount",amount)
                     .executeUpdate();*/
 
-        }
     }
-    public String StuffIntoCar(long price,String picture,long instock,long quantity,String name,String account) {
+  }
+
+  public String StuffIntoCar(long price, String picture, long instock, long
+        quantity, String name, String account) {
         try (Connection connection = sql2oDbHandler.getConnector().open()) {
-            String query = "Insert INTO project.productcar(PRICE,PICTURE,INSTOCK,QUANTITy,PRODUCT_NAME,ACCOUNT) VALUES(:price,:picture,:instock,:quantity,:name,:account)";
+          String query = "Insert INTO project.productcar(PRICE,PICTURE,INSTOCK,QUANTITy,PRODUCT_NAME,ACCOUNT) "
+                +
+                "VALUES(:price,:picture,:instock,:quantity,:name,:account)";
 
-            System.out.println(query);
-            connection.createQuery(query)
-                    .addParameter("price",price)
-                    .addParameter("picture",picture)
-                    .addParameter("instock",instock)
-                    .addParameter("quantity",quantity)
-                    .addParameter("name",name)
-                    .addParameter("account",account)
+          System.out.println(query);
+          connection.createQuery(query)
+                    .addParameter("price", price)
+                    .addParameter("picture", picture)
+                    .addParameter("instock", instock)
+                    .addParameter("quantity", quantity)
+                    .addParameter("name", name)
+                    .addParameter("account", account)
                     .executeUpdate();
-
         }
         return "success";
     }
